9847036acc53e46aca20146f3aa7a1168ec879cc
Jana Johansson <Janajohansson00@hotmail.com>
2023-09-14 12:47:21 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Spelet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hemligt = 7;
+
+            while (true)
+            {
+                Console.WriteLine("Gissa talet");
+                int input = int.Parse(Console.ReadLine());
+
+                if (input == hemligt)
+                {
+                    Console.WriteLine("Du har gissat rätt");
+                    break;
+                }
+                else if (input == hemligt + 1 || input == hemligt - 1)
+                {
+                    Console.WriteLine("Du var nära");
+                }
+                else if (input > hemligt)
+                {
+                    Console.WriteLine("Talet är för högt");
+                }
+                else if (input < hemligt)
+                {
+                    Console.WriteLine("Talet är för lågt");
+                }
+            }
+            Console.WriteLine("Spelet är slut");
+            
+        }
+    }
+}
